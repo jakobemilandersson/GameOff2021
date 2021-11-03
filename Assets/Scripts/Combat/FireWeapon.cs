18bb2ccfc9127935e384ab73f0597deb581699a4
Gustav Larsson <nitrp8@gmail.com>
2021-11-03 17:20:44 +0100

    Add Ammo system to shoot

--- Assets/Scripts/Combat/FireWeapon.cs
+++ Assets/Scripts/Combat/FireWeapon.cs
@@ -7,16 +7,22 @@ public class FireWeapon : MonoBehaviour
     public Animator playerAnimator; 
     [SerializeField]
     private bool automatic = true;
+
+    //Stats of the weapon
     [SerializeField]
     private float attackSpeed = 0.5f;
     [SerializeField]
     private float maxShootRange = 300f;
     [SerializeField]
     private float damage = 100f;   
+    public int maxAmmo = 30;
+    
+    private int currentAmmo = 25;
     public LayerMask playerLayer;
 
     private bool gunReady = true;
 
+    //Prefabs and effects
     public ParticleSystem muzzle;
     public GameObject barrel;
     public GameObject bulletImpactPrefab;
@@ -27,7 +33,7 @@ public class FireWeapon : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentAmmo = maxAmmo;
     }
 
     // Update is called once per frame
@@ -37,23 +43,31 @@ public class FireWeapon : MonoBehaviour
         //Vi kallar shoot().
         if(automatic)
         {
-            if(Input.GetButton("Fire1")&&gunReady)
+            if(Input.GetButton("Fire1")&&gunReady&&currentAmmo>0)
             {
                 Shoot();                
             }               
         }
         else
         {
-            if(Input.GetButtonDown("Fire1")&&gunReady)
+            if(Input.GetButtonDown("Fire1")&&gunReady&&currentAmmo>0)
             {
                 Shoot();                
             }   
+        }
+        
+        //Reload, om R klickas, blir vapnet oanvändbart tills animationen är klar.
+        if(Input.GetButtonDown("Reload"))
+        {
+            gunReady = false;
+            playerAnimator.SetTrigger("Reload");
         }
     }
     void Shoot() //Kallas från detta script när spelaren skjuter.      
     {
         gunReady = false;// gör vapnet oskjutbart
         Invoke(nameof(ResetGunReady),attackSpeed);//Kallar en reset av gunReady efter viss antal tid attackspeed
+        currentAmmo -=1;
         
         
         playerAnimator.SetTrigger("Shoot");//Aktiverar animationen
@@ -75,7 +89,7 @@ public class FireWeapon : MonoBehaviour
             
             //Skapar en kula som effekt om vi är tillräckligt långt bort (bara som cool effekt)
             Vector3 distanceToHitPoint =  (hit.point-transform.position); //räknar ut hur långt bort träffen är
-            if(distanceToHitPoint.magnitude>10)//Om den är tillräckligt långt bort spawnar vi en kula
+            if(distanceToHitPoint.magnitude>5)//Om den är tillräckligt långt bort spawnar vi en kula
             {
                 GameObject bullet = Instantiate(plasmaBulletPrefab,barrel.transform.position,Quaternion.LookRotation(barrel.transform.position - hit.point));
                 Bullet bulletScript = bullet.GetComponent<Bullet>();//Kulan som spawnas har en komponent bullet
@@ -88,5 +102,10 @@ public class FireWeapon : MonoBehaviour
     void ResetGunReady()
     {
         gunReady = true;
+    }
+    public void FinishedReloadAnimation()
+    {
+        ResetGunReady();
+        currentAmmo = maxAmmo;
     }
 }
