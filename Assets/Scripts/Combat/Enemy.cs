e3cd4ffadf346564febb16a80b74fcdf1c00c2d3
Jakob Andersson <jakobemilandersson@gmail.com>
2021-11-05 22:55:01 +0100

    Add simple health-script and temp enemy attack.

--- Assets/Scripts/Combat/Enemy.cs
+++ Assets/Scripts/Combat/Enemy.cs
@@ -7,8 +7,13 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     public float maxHealth = 200;
     public float currentHealth = 5;
+    public int damage = 10;
+    public float attackSpeed = 0.6f;
+    [SerializeField]
+    private float lastAttack;
     void Start()
     {
+        lastAttack = Time.time;
         currentHealth = maxHealth;
     }
 
@@ -23,5 +28,32 @@ public class Enemy : MonoBehaviour
         currentHealth -= damage;
         if(currentHealth<0)
             Debug.Log("I died");
+    }
+
+    void GiveDamage(GameObject target)
+    {
+        Debug.Log("[GiveDamage] target: " + target.name);
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        GameObject collidingObjectRoot = other.gameObject.transform.root.gameObject;
+        if(collidingObjectRoot.tag == "Player")
+        {
+            float _attackTime = Time.time;
+            Debug.Log("[OnCollisionStay] other: " + collidingObjectRoot.name + " | " + collidingObjectRoot.tag + " ~ " + _attackTime);
+            collidingObjectRoot.GetComponent<PlayerHealth>().DecrementHealth(damage);
+            lastAttack = _attackTime;
+        }
+    }
+
+    private void OnCollisionStay(Collision other) {
+        GameObject collidingObjectRoot = other.gameObject.transform.root.gameObject;
+        if(collidingObjectRoot.tag == "Player" && lastAttack + attackSpeed <= Time.time)
+        {
+            float _attackTime = Time.time;
+            Debug.Log("[OnCollisionStay] other: " + collidingObjectRoot.name + " | " + collidingObjectRoot.tag + " ~ " + _attackTime);
+            collidingObjectRoot.GetComponent<PlayerHealth>().DecrementHealth(damage);
+            lastAttack = _attackTime;
+        }
     }
 }
