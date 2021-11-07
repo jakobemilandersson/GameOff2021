7eb4ab634d51f0da1b07892d96a325b197daca88
opi <opirity@gmail.com>
2021-11-07 10:58:51 +0100

    Added enemy healthbar

--- Assets/Scripts/Combat/Enemy.cs
+++ Assets/Scripts/Combat/Enemy.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     // Start is called before the first frame update
     public float maxHealth = 200;
     public float currentHealth = 5;
+    public Slider healthBarSlier;
     void Start()
     {
         currentHealth = maxHealth;
@@ -15,13 +17,13 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        SetHealthBarSlider();
     }
     public void TakeDamage(float damage)
     {
         Debug.Log("I took damage");
         currentHealth -= damage;
-        if(currentHealth<0)
+        if(currentHealth<=0)
             Die();
             
     }
@@ -29,5 +31,15 @@ public class Enemy : MonoBehaviour
     {
         Debug.Log("I died");
         Destroy(gameObject);
+    }
+
+    private void SetHealthBarSlider()
+    {
+        healthBarSlier.value = CalculateHealthPercentage();
+    }
+
+    private float CalculateHealthPercentage()
+    {
+        return ((float)currentHealth / (float)maxHealth) * 100;
     }
 }
