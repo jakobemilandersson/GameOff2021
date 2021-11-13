d3cb4d055fa2d8df7e6fccc3f6badbbd6c06deee
opi <opirity@gmail.com>
2021-11-13 11:39:27 +0100

    ADded hpbar

--- Assets/Scripts/Combat/Enemy.cs
+++ Assets/Scripts/Combat/Enemy.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     // Start is called before the first frame update
     public float maxHealth = 200;
     public float currentHealth = 5;
+    public Slider healthBarSlier;
+
+    public Image healthBarFillImage;
+    public Color maxHealthColor;
+    public Color noHealthColor;
     void Start()
     {
         currentHealth = maxHealth;
@@ -15,13 +21,13 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        SetHealthBarSlider();
     }
     public void TakeDamage(float damage)
     {
         Debug.Log("I took damage");
         currentHealth -= damage;
-        if(currentHealth<0)
+        if(currentHealth<=0)
             Die();
             
     }
@@ -29,5 +35,17 @@ public class Enemy : MonoBehaviour
     {
         Debug.Log("I died");
         Destroy(gameObject);
+    }
+
+    private void SetHealthBarSlider()
+    {
+        float healthPercentage = CalculateHealthPercentage();
+        healthBarSlier.value = healthPercentage;
+        healthBarFillImage.color = Color.Lerp(noHealthColor, maxHealthColor, healthPercentage / 100);
+    }
+
+    private float CalculateHealthPercentage()
+    {
+        return ((float)currentHealth / (float)maxHealth) * 100;
     }
 }
